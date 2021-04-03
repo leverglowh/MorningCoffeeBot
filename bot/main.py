f13181009a8c5a7163041b12c9ad5c2723aca03c
leverglowh <leverglowh@gmail.com>
2021-04-03 21:37:36 +0200

    Handle emojis and puntuations in messages

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,19 @@
 import discord
+import string
+import re
 import os
 
 client = discord.Client()
 partial_keywords = ['good morning', 'good mornin']
+
+def deEmojify(text):
+    regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags = re.UNICODE)
+    return regrex_pattern.sub(r'',text)
 
 
 @client.event
@@ -14,9 +25,21 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    # Ignore all emojis
+    newMessage = deEmojify(message.content).translate(str.maketrans('', '', string.punctuation)).strip()
 
-    if (' ' not in message.content and 'mornin' in message.content.lower()) or any(keyword in message.content.lower() for keyword in partial_keywords):
+    if (' ' not in newMessage and 'mornin' in newMessage.lower()) or any(keyword in newMessage.lower() for keyword in partial_keywords):
         await message.add_reaction(r"☕")
+
+@client.event
+async def on_message_edit(before, after):
+    if before.author == client.user:
+        return
+    # Ignore all emojis and punctuations
+    newMessage = deEmojify(after.content).translate(str.maketrans('', '', string.punctuation)).strip()
+
+    if (' ' not in newMessage and 'mornin' in newMessage.lower()) or any(keyword in newMessage.lower() for keyword in partial_keywords):
+        await after.add_reaction(r"☕")
 
 
 client.run(os.getenv('MORNING_COFFEE_BOT_TOKEN'))
